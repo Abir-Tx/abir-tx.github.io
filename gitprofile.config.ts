02782a8ec81bf93cf94af076003b26933924e16b
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2024-03-19 21:14:28 +0600

    Update the config file with my details
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,14 +2,14 @@
 
 const CONFIG = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (This is the only required config)
+    username: 'abir-tx', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/gitprofile/',
+  base: '/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -17,9 +17,9 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 16, // How many projects to display.
         exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
+          forks: true, // Forked projects will not be displayed if set to true.
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
@@ -33,106 +33,133 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: 'MPM',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'Mass People Management desktop software written in C#. It is a simple app to connect to a database and manage people/data.',
+          imageUrl: '',
+          link: '',
         },
         {
-          title: 'Project Name',
+          title: 'AIUB Discobot',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
+            'The most complete & only bot for American International University of Bangladesh (AIUB). Get auto notice updates & fetch AIUB information',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://cdn.discordapp.com/avatars/970102591155499038/2433de12c3497e39f637e708e0af938d.png?size=256',
+          link: 'https://aiub-discobot.vercel.app/',
+        },
+        {
+          title: 'Sopno IT Solutions Website',
+          description:
+            'The official landing page for the IT center named Sopno IT Solutions',
+          imageUrl:
+            'https://sopnoitsolutions-web.vercel.app/images/sits-logo.png',
+          link: 'https://sopnoitsolutions-web.vercel.app/',
         },
       ],
     },
   },
   seo: {
-    title: 'Portfolio of Ariful Alam',
+    title: 'Portfolio of Mushfiqur Rahman Abir',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    researchGate: '',
-    facebook: '',
+    linkedin: 'mushfiqur-rahman-abir',
+    twitter: 'abir_tx',
+    facebook: 'mushfiqurrohoman.abir',
     instagram: '',
     youtube: '', // example: 'pewdiepie'
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
+    medium: '',
+    dev: 'abirtx',
+    stackoverflow: '13353813/mushfiqur-rahman-abir', // format: userid/username
     skype: '',
     telegram: '',
-    website: 'https://www.arifszn.com',
+    website: 'https://abir-tx.github.io',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'abirtx@yandex.com',
   },
   resume: {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
+    'C++',
     'PHP',
-    'Laravel',
+    'Python',
+    'Linux',
+    'C#',
+    'QT-5',
+    'OpenGL',
+    'Bash',
+    // 'Laravel',
     'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
+    // 'React.js',
+    // 'Node.js',
+    // 'Nest.js',
     'MySQL',
     'PostgreSQL',
     'Git',
     'Docker',
-    'PHPUnit',
+    // 'PHPUnit',
     'CSS',
-    'Antd',
-    'Tailwind',
+    // 'Antd',
+    // 'Tailwind',
+    'Bootstrap',
+    'Java',
+    'Photography',
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'AIUB Photography Club',
+      position: 'Photographer',
+      from: 'February 2022',
       to: 'Present',
       companyLink: 'https://example.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
+      company: 'Mushifuqur Rahman Abir Imagery',
+      position: 'Owner & Photographer',
+      from: 'December 2019',
+      to: 'Present',
+    },
+    {
+      company: 'Sopno IT Solutions',
+      position: 'Web Developer',
+      from: 'December 2021',
+      to: 'Present',
     },
   ],
-  certifications: [
+  /*   certifications: [
     {
       name: 'Lorem ipsum',
       body: 'Lorem ipsum dolor sit amet',
       year: 'March 2022',
       link: 'https://example.com',
     },
-  ],
-  educations: [
+  ], */
+  education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
+      institution: 'American International University-Bangladesh',
+      degree: 'Bachelor of Science',
+      from: '2019',
+      to: '',
+    },
+    {
+      institution: 'Birshreshtha Munshi Abdur Rouf Public College',
+      degree: 'Higher Secondary Certificate (HSC)',
+      from: '2017',
       to: '2019',
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      institution: 'Rajapur High School',
+      degree: 'Secondary School Certificate (SSC)',
+      from: '2011',
+      to: '2017',
     },
   ],
+
   publications: [
     {
       title: 'Publication Title',
@@ -153,22 +180,23 @@ const CONFIG = {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     },
   ],
-  // Display articles from your medium or dev account. (Optional)
+  // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
+    username: 'abirtx',
+    limit: 3, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-XTJQL7VC6J', // Please remove this and use your own tag id or keep it empty
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
-    id: '',
-    snippetVersion: 6,
+    // id: '2617601', //  Please remove this and use your own id or keep it empty
+    // snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'lofi',
+    defaultTheme: 'dracula',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
